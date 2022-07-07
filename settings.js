b102623f7472b7d17a405c600615ea522090d2e7
TechwithAmee1 <107279170+TechwithAmee1@users.noreply.github.com>
2022-07-07 15:22:39 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -32,35 +32,38 @@ global.APIKeys = {
 }
 
 //other
-global.owner = ['94715166712'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
-global.pemilik = ['94715166712'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
-global.premium = ['94715166712'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
-global.pengguna = 'Dark Maker' // Your name ඔබේ නම 
-global.botnma = '💃 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
-global.ownernma = '𝓓𝓪𝓻𝓴 𝓜𝓪𝓴𝓮𝓻' //ownernama,ownername
-global.packname = 'Queen Elisa Sticker' // Sticker package name 
-global.author = 'Dark Maker' // Sticker Autor name 
+global.owner = ['94767453646'] // Owner number , (අයිතිකරුගේ නම්බර් එක )
+global.pemilik = ['94767453646'] //Change  it , ඔබට කැමති නම් වෙනස් කරන්න
+global.premium = ['94767453646'] //Change it ඔබට කැමතිනම් වෙනස් කරන්න 
+global.pengguna = 'Amiya Pro' // Your name ඔබේ නම 
+global.botnma = '💃 𝗔𝗺𝗶𝘆𝗮 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐁𝐨𝐭 💃' // Your bot name , ඔබගෙ බොට්ගේ නම 
+global.ownernma = '𝓪𝓶𝓲𝔂𝓪 𝓹𝓻𝓸' //ownernama,ownername
+global.packname = 'Amiya Queen Elisa Sticker' // Sticker package name 
+global.author = 'Amiya Pro' // Sticker Autor name 
 global.sessionName = 'session'
 global.prefa = ['#','!','/',''] 
 global.sp = '🔵'
-// Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` ` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
+// Alive massage , 
+// Alive massage , ඔබේ බොගේ alive massage  එක මෙහි ඇති ` බොට් ඔන්ලයින් ඉන්නවා මොකො වෙන්න ඕනි.
+ගහල මෙනුව ගන්නෙ .menu ගහල 
+සින්දු ගන්නෙ .yt ගහල` මෙම ලකුනු ඇතුලේ ඔබට අවශ්‍ය ඔනිම දෙයක් Type කරන්න.💃
 global.alivelogo = `https://telegra.ph/file/1a2dee71baa975091620d.jpg`
 global.alive =`Hello i am alive now
 
 Download song type .yt 
 Thanks fro using...`
-// Welcome massage  ඔබට කැමති නම් මෙයට ඔබෙ welcome massage  එක යොදන්න 
+// Welcome massage  ඔබට කැමති නම් මෙයට ඔබෙ welcome massage  එක යොදන්න
 global.welcome = `
 🌀 Thanks Fro using sl zim bot
 ✾ Some bugs fixing
 😼♥️
 `
 // වෙල්කම් එක යවන්න ඔනි නම් add කියලා දාන්න 
-global.sendwelcome = `add`
+global.sendwelcome = ``
 // Good bye එක ඔනි නම් remove කියලා දාන්න
-global.sendgoodbye = `remove`
+global.sendgoodbye = ``
 // Caption 
-global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
+global.cap = `© 𝐆𝐞𝐧𝐞𝐫𝐚𝐭𝐞𝐝 𝐁𝐲 𝗔𝗺𝗶𝘆𝗮 𝐐𝐮𝐞𝐞𝐧 𝐄𝐥𝐢𝐬𝐚 𝐌𝐃`
 global.mess = {
     success: '✅ Done!',
     admin: 'you must be admin to use this cmd!',
